6866a01778ced607e4f96db67b7280e6c325cfd6
yasapuramvalmikinayana <yasapuramvalmikinayana@gmail.com>
2022-07-18 14:55:53 +0530

    files added

---
+++ src/com/xworkz/Collection/CollectionDemo.java
@@ -0,0 +1,22 @@
+package com.xworkz.Collection;
+
+import java.util.ArrayList;
+
+public class CollectionDemo {
+
+		public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+	    al.add(1);
+	    al.add(2);
+	    al.add(3);
+	    al.add(4);
+	    al.add(5);
+	    
+	    System.out.println(al);
+	    
+		}
+
+	}
+	
+
+
---
+++ src/com/xworkz/Collection/CollectionDemo2.java
@@ -0,0 +1,19 @@
+package com.xworkz.Collection;
+
+import java.util.ArrayList;
+
+public class CollectionDemo2 {
+
+		public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add('A');
+		al.add('B');
+		al.add('C');
+		al.add('D');
+		al.add('D');
+		System.out.println(al);
+
+		}
+
+	}
+
